55918181f0673e4de0c045b4e03341edf5f007cd
Alex Barry <alex.barry@gmail.com>
2018-02-08 16:00:32 -0500

    Better demo that actually handles HTTP errors properly.

--- src/demo.js
+++ src/demo.js
@@ -5,12 +5,21 @@ import WaitForIt from '.';
 const promise = new Promise(resolve => {
   setTimeout(resolve, 5000)
 })
-  .then(() => fetch('https://jsonplaceholder.typicode.com/users'))
-  .then(res => res.json());
+  .then(() => fetch('https://jsonplaceholder.typicode.com/users/error'))
+  .then(res => {
+    if (res.status >= 400) {
+      return Promise.reject({
+        message: `HTTP ${res.status}`,
+        response: res,
+      })
+    }
+    return res.json()
+  });
 
 const App = () => (
   <WaitForIt
     promise={promise}
+    renderError={err => [err.message, JSON.stringify(err.response.json())].join(' ')}
   />
 );
 
